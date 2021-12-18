66069ba05cfaa5a10f346a098f92cb4b788a43b2
ikachko <ilyakachko@gmail.com>
2021-12-18 10:53:39 +0200

    script update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
-ACCOUNT=kkswap.testnet
-near deploy $ACCOUNT res/near_orderbook.wasm new "[10,\"kstasi.testnet\",true,10,10]"
-# near deploy $ACCOUNT res/near_orderbook.wasm new "[ft.examples.testnet, keku.testnet]"
+ACCOUNT=kkdex.testnet
+# near deploy $ACCOUNT res/near_orderbook.wasm new "[10,\"kstasi.testnet\",true,10,10]"
+near deploy $ACCOUNT res/near_orderbook.wasm new "[\"ft.examples.testnet\", \"keku.testnet\"]"
